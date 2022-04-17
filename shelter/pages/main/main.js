2d2bb357ebce87d3c3f20e001c5172f292f5b5ec
Vasiliy <>
2022-04-17 21:30:45 +0300

    feat: add adaptive layout for shelter main page

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -13,4 +13,25 @@ for(let anchor of anchors){
   }
 }
 
+const mobileNavBtn = document.querySelector('.mobileBtn');
+const mobileNav = document.querySelector('.nav');
+mobileNavBtn.onclick = () => {
+  const input = () => {
+    setTimeout(() => mobileNav.classList.toggle('nav_active'), 10);
+    mobileNav.style.display = 'flex';
+    mobileNavBtn.classList.toggle('mobileBtn_active')
+  }
+  const output = () => {
+    setTimeout(() => mobileNav.style.display = 'none', 400);
+    mobileNav.classList.toggle('nav_active');
+    mobileNavBtn.classList.toggle('mobileBtn_active')
+  }
+
+  mobileNav.className.includes('nav_active') 
+  ? 
+  output()
+  : 
+  input();
+}
+
 })()
