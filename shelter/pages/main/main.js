14e8ae0725614f358b220c5d8ee08ff6adf282b0
Vasiliy <>
2022-04-18 18:36:41 +0300

    feat: add adaptive layout for our pets and mobile menu

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -4,34 +4,46 @@ const anchors = document.querySelectorAll('a[href*="#"]');
 
 for(let anchor of anchors){
   anchor.onclick = function(event){
-    event.preventDefault();
-    const allAnchors = anchor.getAttribute('href');
-    document.querySelector(''+ allAnchors).scrollIntoView({
-      behavior:'smooth',
-      block:'start',
-    });
+    let allAnchors = anchor.getAttribute('href');
+    let smoothFlow = (event, anchors) => {
+      event.preventDefault();
+      document.querySelector(''+ anchors).scrollIntoView({
+        behavior:'smooth',
+        block:'start',
+      });
+    }
+
+    if(event.target.className.includes('mainContent__btn')) {
+      smoothFlow(event, allAnchors)
+    }else {
+      mobileBtnSwitcher();
+      smoothFlow(event, allAnchors)
+    }
+    
   }
 }
 
 const mobileNavBtn = document.querySelector('.mobileBtn');
 const mobileNav = document.querySelector('.nav');
-mobileNavBtn.onclick = () => {
-  const input = () => {
-    setTimeout(() => mobileNav.classList.toggle('nav_active'), 10);
-    mobileNav.style.display = 'flex';
-    mobileNavBtn.classList.toggle('mobileBtn_active')
-  }
-  const output = () => {
-    setTimeout(() => mobileNav.style.display = 'none', 400);
-    mobileNav.classList.toggle('nav_active');
-    mobileNavBtn.classList.toggle('mobileBtn_active')
-  }
-
+const input = () => {
+  setTimeout(() => mobileNav.classList.toggle('nav_active'), 10);
+  mobileNav.style.display = 'flex';
+  mobileNavBtn.classList.toggle('mobileBtn_active')
+}
+const output = () => {
+  setTimeout(() => mobileNav.style.display = 'none', 400);
+  mobileNav.classList.toggle('nav_active');
+  mobileNavBtn.classList.toggle('mobileBtn_active')
+}
+const mobileBtnSwitcher = () => {
   mobileNav.className.includes('nav_active') 
   ? 
   output()
   : 
   input();
+}
+mobileNavBtn.onclick = () => {
+  mobileBtnSwitcher()
 }
 
 })()
--- shelter/pages/pets/main.js
+++ shelter/pages/pets/main.js
@@ -1,19 +1,43 @@
 (function() {
 
-const anchors = document.querySelectorAll('a[href*="#"]');
+  const anchors = document.querySelectorAll('a[href*="#"]');
 
-for(let anchor of anchors){
-  if(anchor.hash == '#help') {
-    console.log('got it')
-  }else {
-    anchor.onclick = function(event){
-      event.preventDefault();
-      const allAnchors = anchor.getAttribute('href');
-      document.querySelector(''+ allAnchors).scrollIntoView({
-        behavior:'smooth',
-        block:'start',
-      });
+  for(let anchor of anchors){
+    if(anchor.hash == '#help') {
+      console.log('got it')
+    }else {
+      anchor.onclick = function(event){
+        mobileBtnSwitcher();
+        event.preventDefault();
+        const allAnchors = anchor.getAttribute('href');
+        document.querySelector(''+ allAnchors).scrollIntoView({
+          behavior:'smooth',
+          block:'start',
+        });
+      }
     }
   }
-}
+
+  const mobileNavBtn = document.querySelector('.mobileBtn');
+  const mobileNav = document.querySelector('.nav');
+  const input = () => {
+    setTimeout(() => mobileNav.classList.toggle('nav_active'), 10);
+    mobileNav.style.display = 'flex';
+    mobileNavBtn.classList.toggle('mobileBtn_active')
+  }
+  const output = () => {
+    setTimeout(() => mobileNav.style.display = 'none', 400);
+    mobileNav.classList.toggle('nav_active');
+    mobileNavBtn.classList.toggle('mobileBtn_active')
+  }
+  const mobileBtnSwitcher = () => {
+    mobileNav.className.includes('nav_active') 
+    ? 
+    output()
+    : 
+    input();
+  }
+  mobileNavBtn.onclick = () => {
+    mobileBtnSwitcher()
+  }
 })()
