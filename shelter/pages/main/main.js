50543cd113d4f10f65cf168b159abf7053b0537f
Vasiliy <>
2022-04-23 08:26:44 +0300

    feat: add burger menu

--- shelter/pages/main/main.js
+++ shelter/pages/main/main.js
@@ -23,27 +23,39 @@ for(let anchor of anchors){
   }
 }
 
-const mobileNavBtn = document.querySelector('.mobileBtn');
-const mobileNav = document.querySelector('.nav');
-const input = () => {
-  setTimeout(() => mobileNav.classList.toggle('nav_active'), 10);
-  mobileNav.style.display = 'flex';
-  mobileNavBtn.classList.toggle('mobileBtn_active')
-}
-const output = () => {
-  setTimeout(() => mobileNav.style.display = 'none', 400);
-  mobileNav.classList.toggle('nav_active');
-  mobileNavBtn.classList.toggle('mobileBtn_active')
-}
-const mobileBtnSwitcher = () => {
-  mobileNav.className.includes('nav_active') 
-  ? 
-  output()
-  : 
-  input();
-}
-mobileNavBtn.onclick = () => {
-  mobileBtnSwitcher()
-}
+  //Burger menu
+
+  const mobileNavBtn = document.querySelector('.mobileBtn');
+  const mobileNav = document.querySelector('.nav');
+  const body = document.querySelector('.page');
+  const overlay = document.querySelector('.overlay');
+
+  const input = () => {
+    setTimeout(() => mobileNav.classList.toggle('nav_active'), 10);
+    mobileNav.style.display = 'flex';
+    mobileNavBtn.classList.toggle('mobileBtn_active');
+    body.classList.toggle('page_unactive');
+    overlay.style.display = 'block';
+  }
+  const output = () => {
+    setTimeout(() => mobileNav.style.display = 'none', 400);
+    mobileNav.classList.toggle('nav_active');
+    mobileNavBtn.classList.toggle('mobileBtn_active');
+    body.classList.toggle('page_unactive');
+    overlay.style.display = 'none';
+  }
+  const mobileBtnSwitcher = () => {
+    mobileNav.className.includes('nav_active') 
+    ? 
+    output()
+    : 
+    input();
+  }
+  mobileNavBtn.onclick = () => {
+    mobileBtnSwitcher()
+  }
+  overlay.onclick = () => {
+    mobileBtnSwitcher()
+  }
 
 })()
